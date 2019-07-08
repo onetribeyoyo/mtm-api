4cf995d201ccf6c3b49ee4a068750720ebafeabc
Andy Miller <onetribeyoyo@gmail.com>
2019-07-07 19:46:13 -0500

    adding mongo.

--- mtm-api.groovy
+++ mtm-api.groovy
@@ -1,8 +1,20 @@
 @Grapes([
   @Grab('io.ratpack:ratpack-groovy:1.6.1'),
+  @Grab("org.grails:grails-datastore-gorm-mongodb:6.1.1.RELEASE"),
 ])
 
 import static ratpack.groovy.Groovy.ratpack
+
+import ratpack.http.Status
+import ratpack.jackson.Jackson
+import static ratpack.jackson.Jackson.json
+
+import grails.gorm.annotation.Entity
+import org.grails.datastore.mapping.mongo.MongoDatastore
+import org.bson.types.ObjectId
+
+
+def datastore = new MongoDatastore(Product)
 
 ratpack {
 
@@ -24,26 +36,50 @@ ratpack {
         // product api
         //
         get("product/count") {
-            render "TODO: GET product/count NOT IMPLEMENTED"
+            Long count = Product.count()
+            render json(count)
         }
 
         get("products") {
-            render "TODO: GET products NOT IMPLEMENTED"
+            // GORM supports paginiation; e.g. `products ? offset=2 & max=3`.
+            Long offset = request.queryParams.offset?.toLong() ?: 0
+            Long max = request.queryParams.max?.toLong() ?: 10
+            def products = Product.list(offset:offset, max:max)
+            render json( products?.collect{ product -> product.toMap() } )
         }
 
         path("product/:productId") {
             byMethod {
                 delete {
-                    render "TODO: DELETE product/:productId NOT IMPLEMENTED"
+                    def product = Product.get(new ObjectId(pathTokens.productId))
+                    if (product) {
+                        product.delete(flush:true)
+                        response.status(Status.NO_CONTENT)
+                        render json(product.toMap())
+                    } else {
+                        response.status(Status.NOT_FOUND)
+                        render "${response.status.code}"
+                    }
                 }
                 get {
-                    render "TODO: GET product/:productId NOT IMPLEMENTED"
+                    def product = Product.get(pathTokens.productId)
+                    if (product) {
+                        render json(product.toMap())
+                    } else {
+                        response.status(Status.NOT_FOUND)
+                        render "${response.status.code}"
+                    }
                 }
                 patch {
                     render "TODO: PATCH product/:productId NOT IMPLEMENTED"
                 }
                 post {
-                    render "TODO: POST product/:productId NOT IMPLEMENTED"
+                    context.parse(Jackson.fromJson(Map)).then { data ->
+                        Product p = new Product(name:data.name)
+                        // TODO: validation!!!!
+                        p.save(flush:true)
+                        render "/product/${p.id}"
+                    }
                 }
                 put {
                     render "TODO: PUT product/:productId NOT IMPLEMENTED"
@@ -54,3 +90,20 @@ ratpack {
     }
 
 }
+
+// --------------------------------------------------------------------------------------------
+
+@Entity
+class Product {
+    ObjectId id
+    String name
+
+    def toMap() {
+        [
+            id: id?.toString(),
+            name: name
+        ]
+    }
+}
+
+// --------------------------------------------------------------------------------------------
